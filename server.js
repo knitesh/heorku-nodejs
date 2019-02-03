88b174c915800f30fcfafbff5d619c32b2410edb
Kumar Nitesh <contact@kumar-nitesh.com>
2019-02-02 23:02:30 -0500

    Using port

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var express = require("express");
 
 var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 app.get("/", (req, res) => {
   res.send("Hello from Server");
 });
-app.listen(80, () => {
+app.listen(port, () => {
   console.log("Server started on 8080");
 });
